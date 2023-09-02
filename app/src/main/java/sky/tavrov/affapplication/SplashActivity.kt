0477992ae6bf8c882d2a49255cee0459cda86a83
Oleh Kholiavchuk <oleh.khol@gmail.com>
2023-09-02 19:54:03 +0300

    12. Splash Screen

---
+++ app/src/main/java/sky/tavrov/affapplication/SplashActivity.kt
@@ -0,0 +1,36 @@
+package sky.tavrov.affapplication
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen") // TODO https://developer.android.com/guide/topics/ui/splash-screen
+@Suppress("DEPRECATION")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                finish()
+            },
+            2500
+        )
+    }
+}
