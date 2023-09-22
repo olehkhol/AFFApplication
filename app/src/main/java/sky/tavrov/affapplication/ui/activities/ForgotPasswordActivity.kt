ade1c21f5d3807eba891dfbefaa078389b74c07d
Oleh Kholiavchuk <oleh.khol@gmail.com>
2023-09-22 12:02:44 +0300

    30. The Forgot Password Implementation and Test

--- app/src/main/java/sky/tavrov/affapplication/ui/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/sky/tavrov/affapplication/ui/activities/ForgotPasswordActivity.kt
@@ -1,6 +1,8 @@
 package sky.tavrov.affapplication.ui.activities
 
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import sky.tavrov.affapplication.R
 import sky.tavrov.affapplication.databinding.ActivityForgotPasswordBinding
 
@@ -17,11 +19,38 @@ class ForgotPasswordActivity : BaseActivity() {
     }
 
     private fun setupActionBar() {
-        setSupportActionBar(binding.toolbarForgotPasswordActivity)
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        with(binding) {
+            setSupportActionBar(toolbarForgotPasswordActivity)
+            supportActionBar?.apply {
+                setDisplayHomeAsUpEnabled(true)
+                setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+            }
+            toolbarForgotPasswordActivity.setNavigationOnClickListener { onBackPressed() }
+            btnSubmit.setOnClickListener {
+                val email = etEmail.text.toString().trim { it <= ' ' }
+                if (email.isEmpty()) {
+                    showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                } else {
+                    showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                    FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                        .addOnCompleteListener { task ->
+
+                            hideProgressDialog()
+
+                            if (task.isSuccessful) {
+                                Toast.makeText(
+                                    this@ForgotPasswordActivity,
+                                    resources.getString(R.string.email_sent_success),
+                                    Toast.LENGTH_LONG
+                                ).show()
+
+                                finish()
+                            } else {
+                                showErrorSnackBar(task.exception?.message.toString(), true)
+                            }
+                        }
+                }
+            }
         }
-        binding.toolbarForgotPasswordActivity.setNavigationOnClickListener { onBackPressed() }
     }
 }
