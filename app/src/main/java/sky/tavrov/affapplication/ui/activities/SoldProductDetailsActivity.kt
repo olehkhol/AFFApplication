4a8b55b1e3e052f9a1a7d295308a6b1efc335e4c
Oleh Kholiavchuk <oleh.khol@gmail.com>
2023-10-04 23:14:14 +0300

    119. Passing Details from The Sold Products Screen

--- app/src/main/java/sky/tavrov/affapplication/ui/activities/SoldProductDetailsActivity.kt
+++ app/src/main/java/sky/tavrov/affapplication/ui/activities/SoldProductDetailsActivity.kt
@@ -1,12 +1,69 @@
 package sky.tavrov.affapplication.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import sky.tavrov.affapplication.R
+import android.view.View
+import sky.tavrov.affapplication.data.models.SoldProduct
+import sky.tavrov.affapplication.databinding.ActivitySoldProductDetailsBinding
+import sky.tavrov.affapplication.ui.utils.Constants
+import sky.tavrov.affapplication.ui.utils.GlideLoader
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class SoldProductDetailsActivity : AppCompatActivity() {
+class SoldProductDetailsActivity : BaseActivity() {
+
+    private val binding by lazy { ActivitySoldProductDetailsBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sold_product_details)
+
+        with(binding) {
+            setContentView(root)
+            setupActionBar(toolbarSoldProductDetailsActivity)
+
+            var productDetails = SoldProduct()
+            if (intent.hasExtra(Constants.EXTRA_SOLD_PRODUCT_DETAILS)) {
+                productDetails = intent.getParcelableExtra(Constants.EXTRA_SOLD_PRODUCT_DETAILS)!!
+            }
+            setupUI(productDetails)
+        }
+    }
+
+    private fun setupUI(productDetails: SoldProduct) {
+        with(binding) {
+            tvSoldProductDetailsId.text = productDetails.order_id
+            val dateFormat = "dd MMM yyyy HH:mm"
+            val formatter = SimpleDateFormat(dateFormat, Locale.getDefault())
+
+            val calendar: Calendar = Calendar.getInstance()
+            calendar.timeInMillis = productDetails.order_date
+            tvSoldProductDetailsDate.text = formatter.format(calendar.time)
+
+            GlideLoader(this@SoldProductDetailsActivity).loadProductPicture(
+                productDetails.image,
+                ivProductItemImage
+            )
+            tvProductItemName.text = productDetails.title
+            tvProductItemPrice.text = "$${productDetails.price}"
+            tvSoldProductQuantity.text = productDetails.sold_quantity
+
+            tvSoldDetailsAddressType.text = productDetails.address.type
+            tvSoldDetailsFullName.text = productDetails.address.name
+            tvSoldDetailsAddress.text =
+                "${productDetails.address.address}, ${productDetails.address.zipCode}"
+            tvSoldDetailsAdditionalNote.text = productDetails.address.additionalNote
+
+            if (productDetails.address.otherDetails.isNotEmpty()) {
+                tvSoldDetailsOtherDetails.visibility = View.VISIBLE
+                tvSoldDetailsOtherDetails.text = productDetails.address.otherDetails
+            } else {
+                tvSoldDetailsOtherDetails.visibility = View.GONE
+            }
+            tvSoldDetailsMobileNumber.text = productDetails.address.mobileNumber
+
+            tvSoldProductSubTotal.text = productDetails.sub_total_amount
+            tvSoldProductShippingCharge.text = productDetails.shipping_charge
+            tvSoldProductTotalAmount.text = productDetails.total_amount
+        }
     }
 }
